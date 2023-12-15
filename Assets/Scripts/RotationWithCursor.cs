7859dd7215d61216508b75912ee9c50344ad0cee
avnish32 <avnishbansal9@gmail.com>
2023-12-15 11:21:15 +0000

    P1 rotation implemented

--- Assets/Scripts/RotationWithCursor.cs
+++ Assets/Scripts/RotationWithCursor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class RotationWithCursor : MonoBehaviour
@@ -9,14 +10,34 @@ public class RotationWithCursor : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField]
     private Camera cam;
+
+    [SerializeField]
+    private Sprite down;
+    [SerializeField]
+    private Sprite up;
+    [SerializeField]
+    private Sprite left;
+    [SerializeField]
+    private Sprite right;
 
     private float rotationInDegrees;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
     }
 
     // Update is called once per frame
@@ -27,13 +48,32 @@ public class RotationWithCursor : MonoBehaviour
         Vector2 posDir = cursorScreenPos - rb.position;
         double rotationInRadians = Math.Atan2(posDir.y, posDir.x);
         rotationInDegrees = (float) rotationInRadians * Mathf.Rad2Deg;
+
+        rotationInDegrees += 90f;
+        if (rotationInDegrees < -45f || rotationInDegrees >= 225f)
+        {
+            spriteRenderer.sprite = left;
+        }
+        else if (rotationInDegrees >= -45f && rotationInDegrees < 45f)
+        {
+            spriteRenderer.sprite = down;
+        }
+        else if (rotationInDegrees >= 45f && rotationInDegrees < 135f)
+        {
+            spriteRenderer.sprite = right;
+        }
+        else
+        {
+            spriteRenderer.sprite = up;
+        }
     }
 
     private void FixedUpdate()
     {
         if (rb != null)
         {
-            rb.rotation = rotationInDegrees + 90f;
+            rb.rotation = rotationInDegrees;
+            //Debug.Log("P1 rotation: " + rotationInDegrees);
         }
     }
 }
